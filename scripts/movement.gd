4028458656713fc3e4e9884f2b5f938a75d7e075
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-07-12 09:12:24 +1000

    fix movement and change gravity

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -8,29 +8,33 @@ const UP_DIRECTION := Vector2.UP # change this to allow walking on walls or the 
 @export var speed := 600.0 # := means that the value and TYPE are equal
 @export var jump_strength := 700.0 # how strong a jump is (the velocity added)
 @export var maximum_jumps := 2 # to allow for double jumping
-@export var double_jump_strength := 1200.0 # a weaker jump strength for double jumps
-
+@export var double_jump_strength := 600.0 # a weaker jump strength for double jumps
+var was_on_floor := false
+var jumps_made := 0
 
 func _physics_process(delta: float) -> void:
 	# variables
-	var jumps_made := 0
 	var is_falling := velocity.y > 0.0 and not is_on_floor()
 	var is_jumping := Input.is_action_just_pressed("jump") and (is_on_floor() or !coyote_time.is_stopped()) # jumps if key is pressed and is on floor or coyote time hasn't ended
-	var is_double_jumping := Input.is_action_just_pressed("jump") and is_on_floor() and coyote_time.is_stopped()
+	var is_double_jumping := Input.is_action_just_pressed("jump") and is_falling and coyote_time.is_stopped()
 	var is_jump_cancelled := Input.is_action_just_released("jump") and velocity.y < 0.0
 	var is_idle := is_on_floor() and is_zero_approx(velocity.x)
 	var is_running := is_on_floor() and not is_zero_approx(velocity.x)
-	
 	
 	var direction := Input.get_axis("move_left", "move_right") # move_left is negative and move_right is positive
 
 	velocity.x = direction * speed
 	
+	if not is_on_floor() and was_on_floor:
+		coyote_time.start()
+	
+	was_on_floor = is_on_floor()
+		
+	
 	# handle jumps
 	if is_jumping:
 		jumps_made += 1
 		velocity.y = -jump_strength
-		coyote_time.start()
 	elif is_double_jumping:
 		jumps_made += 1
 		if jumps_made <= maximum_jumps:
