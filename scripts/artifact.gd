818b03a1b92b48d2ad737a2af6b03bd4c7378c99
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-07-14 11:04:11 +1000

    added artifacts

---
+++ scripts/artifact.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var artifactNode: Area2D
+
+@export_enum("prehistoric", "ancient_egypt", "ancient_greece", "imperial_china", "medieval_europe", "renaissance", "industrial_revolution", "prohibition", "world_wars", "modern_day", "near_future", "far_future")
+var timePeriod: String
+
+func _on_body_entered(body: Node2D) -> void:
+	print("you collected an artifact!")
+	Globals.add_artifact(timePeriod)
+	Globals.print_artifacts(timePeriod)
+	artifactNode.queue_free()
---
+++ scripts/globals.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var artifacts_collected := {
+	"prehistoric": 0,
+	"ancient_egypt": 0,
+	"ancient_greece": 0,
+	"imperial_china": 0,
+	"medieval_europe": 0,
+	"renaissance": 0,
+	"industrial_revolution": 0,
+	"prohibition": 0,
+	"world_wars": 0,
+	"modern_day": 0,
+	"near_future": 0,
+	"far_future": 0
+}
+
+func add_artifact(time_period: String):
+	if artifacts_collected.has(time_period):
+		artifacts_collected[time_period] += 1
+
+func print_artifacts(time_period: String):
+	if artifacts_collected.has(time_period):
+		print(artifacts_collected.get(time_period))
+	else:
+		print("time period %s not found" %[str(time_period)])
