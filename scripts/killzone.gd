a2c66452f0bf5e8aeb8d32f9764f84421d9df27d
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-07-15 22:08:10 +1000

    add killzone to level 1

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,10 +3,12 @@ extends Area2D
 @onready var slowdown_time: Timer = $SlowdownTime
 
 func _on_body_entered(body: Node2D) -> void:
-	body.get_node("CollisionShape2D").queue_free()
-	Engine.time_scale = 0.25 # slow down the game to 1/4 speed
-	slowdown_time.start()
-	
+	if body.name == "Player":
+		body.get_node("CollisionShape2D").queue_free()
+		Engine.time_scale = 0.25 # slow down the game to 1/4 speed
+		slowdown_time.start()
+		
 
 func _on_slowdown_time_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
