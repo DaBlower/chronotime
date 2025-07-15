dd8692d9ccac75d0ecba44a0a3064d9969c583f0
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-07-15 12:28:29 +1000

    add optional check quotas option for next scene scene

--- scripts/artifact.gd
+++ scripts/artifact.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-@export var artifactNode: Area2D
 
 @export_enum("prehistoric", "ancient_egypt", "ancient_greece", "imperial_china", "medieval_europe", "renaissance", "industrial_revolution", "prohibition", "world_wars", "modern_day", "near_future", "far_future")
 var timePeriod: String
@@ -9,4 +8,4 @@ func _on_body_entered(body: Node2D) -> void:
 	print("you collected an artifact!")
 	Globals.add_artifact(timePeriod)
 	Globals.print_artifacts(timePeriod)
-	artifactNode.queue_free()
+	queue_free()
--- scripts/next_scene.gd
+++ scripts/next_scene.gd
@@ -3,9 +3,13 @@ extends Area2D
 @export_enum("prehistoric", "ancient_egypt", "ancient_greece", "imperial_china", "medieval_europe", "renaissance", "industrial_revolution", "prohibition", "world_wars", "modern_day", "near_future", "far_future")
 var timePeriod: String
 @export var nextScene: PackedScene
+@export var checkQuotas: bool
 
 func _on_body_entered(body: Node2D) -> void:
-	if Globals.check_quotas(timePeriod):
-		get_tree().change_scene_to_packed(nextScene)
+	if checkQuotas:
+		if Globals.check_quotas(timePeriod):
+			get_tree().change_scene_to_packed(nextScene)
+		else:
+			print("Not enough artifacts!")
 	else:
-		print("Not enough artifacts!")
+		get_tree().change_scene_to_packed(nextScene)
