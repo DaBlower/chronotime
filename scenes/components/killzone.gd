fd81c78ed95dab1faf75c10c768e5f08bce4ede9
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-07-15 21:43:27 +1000

    add basic killzone

--- scenes/components/killzone.gd
+++ scenes/components/killzone.gd
@@ -1,6 +1,12 @@
 extends Area2D
 
-
+@onready var slowdown_time: Timer = $SlowdownTime
 
 func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	body.get_node("CollisionShape2D").queue_free()
+	Engine.time_scale = 0.25 # slow down the game to 1/4 speed
+	slowdown_time.start()
+	
+
+func _on_slowdown_time_timeout() -> void:
+	get_tree().reload_current_scene()
