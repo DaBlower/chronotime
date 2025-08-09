3af1fbeae01abfb66cb3779abfaf2d09d179d4fd
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-08-09 21:52:28 +1000

    add more dialogue and fix bugs

--- scenes/teleport.gd
+++ scenes/teleport.gd
@@ -2,7 +2,13 @@ extends Area2D
 
 @export var Coordinates: Vector2
 @onready var player: CharacterBody2D = $"../../Player"
+@onready var camera: Camera2D = player.get_node("camera")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
+		camera.position_smoothing_speed = 0.4
 		player.teleport(Coordinates)
+		await get_tree().create_timer(3)
+		camera.position_smoothing_speed = 1.7
+		await get_tree().create_timer(3.7).timeout
+		camera.position_smoothing_speed = 5.0
--- scripts/artifact.gd
+++ scripts/artifact.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var artifact_bar: CanvasLayer = $"../../ArtifactBar"
 @onready var player: CharacterBody2D = $"../../Player"
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 @export_enum("prehistoric", "ancient_egypt", "ancient_greece", "imperial_china", "medieval_europe", "renaissance", "industrial_revolution", "prohibition", "world_wars", "modern_day", "near_future", "far_future")
 var timePeriod: String
@@ -10,7 +11,10 @@ var timePeriod: String
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		print("DEBUG: you collected an artifact!")
+		audio_stream_player_2d.play(0.0)
 		Globals.add_artifact(timePeriod)
 		Globals.print_artifacts(timePeriod)
 		artifact_bar.updateArtifacts(Globals.return_artifacts(timePeriod))
+		audio_stream_player_2d.play()
+		await audio_stream_player_2d.finished
 		queue_free()
